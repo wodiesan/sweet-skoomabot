5e710deb719ff078d5b148fcf6e3270a2ae8e839
wodiesan <wodiesan@gmail.com>
2015-06-18 13:42:39 -0400

    Working heatmaps.

--- skoomabot.py
+++ skoomabot.py
@@ -11,16 +11,15 @@ This script uses Reddit's Python API and Plot.ly in order to visualize
 
 TODO:
 1. Better exceptions.
-2. Create individual charts based on category.
-3. Heatmap troubleshooting.
+2. Create individual charts based on sub-category.
 4. Refactor and reorg.
 
 FUTURE:
 5. Tie it to a db (Google Sheets).
 """
 
-import druggiebot_config as dbc
-# import druggiebot_google_func as dbgf
+import skoomabot_config as dbc
+# import skoomabot_google_func as dbgf
 import praw
 from plotly.graph_objs import Bar, Data, Heatmap, Layout, Figure
 import plotly.tools as tls
@@ -29,7 +28,7 @@ import plotly.plotly as viz
 import time
 
 # Identify script to Reddit.
-user_agent = 'Visualize posts to /r/Drugs. Version 0.1'
+user_agent = 'Visualize posts to /r/Drugs. Version 0.3'
 r = praw.Reddit(user_agent=user_agent)
 subreddit = r.get_subreddit('Drugs', fetch=True)
 post_limit = 1000
@@ -46,8 +45,10 @@ for submission in subreddit.get_new(limit=post_limit):
 
     # Fix dbc.checked_post to check for values in different keys.
     if post.id_num not in dbc.checked_post and post._match(dbc.drug_dict):
-        slang = post._category(dbc.drug_dict, dbc.plotly_dict, post.title)
         # slang returns None if the match is under 3 characters in legnth.
+        slang = post._category(dbc.drug_dict, dbc.py_dict_drug, dbc.py_list_drug, dbc.py_matrix_drug, post.title)
+        setting = post._category(dbc.setting_dict, dbc.py_dict_set, dbc.py_list_set, dbc.py_matrix_set, post.title)
+
         if not slang:
             dbc.checked_post['neg'].append(post.id_num)
         else:
@@ -61,43 +62,34 @@ for submission in subreddit.get_new(limit=post_limit):
 
     # Quick and dirty solution to getting the rest of the charts.
     # TODO: Revisit after presentation.
-    setting = post._category(dbc.setting_dict, dbc.plotly_dict_set, post.title)
+    # setting = post._category(dbc.setting_dict, dbc.py_dict_set, dbc.py_list_set, dbc.py_matrix_set, post.title)
 
-    # if post.id_num not in dbc.checked_post and post._match(dbc.drug_dict):
-    #     slang = post._category(dbc.drug_dict, post.title)
-    #     # slang returns None if the match is under 3 characters in legnth.
-    #     if slang:
-    #         dbc.checked_post['pos'].append(post.id_num)
-    #         # TURN submission.id into KEY, and category matches values.
-    #         print 'Post {} contains a match for {}:'.format(post.id_num, slang)
-    #         print 'Title: {}\n'.format(post.title)
 
-    #     else:
-    #         dbc.checked_post['neg'].append(post.id_num)
-
-print '\nThere are {} out of {} titles that contain a match.'.format(len(dbc.checked_post['pos']), post_limit)
+print '\nThere are {} out of {} titles that contain a slang match.'.format(len(dbc.checked_post['pos']), post_limit)
 print 'This leaves {} out of {} titles without matches.'.format(len(dbc.checked_post['neg']), post_limit)
 print '\nBot ended at {}'.format(time.time())
 
-# Testing plot.ly using unordered dictionary broken down into key list and val list.
+# Testing plot.ly using dict broken down into key list and val list.
 print 'plotly plot points:\n'
 
-# sorted_list = sorted(dbc.plotly_dict.items())
-# print  '\nunsorted:'
-# print dbc.plotly_dict
+# Get the x and y axis. Perhaps a function to do this?
+# mainviz_xaxis = dbc.py_dict_drug.keys()
+mainviz_xaxis = dbc.py_list_drug
+mainviz_yaxis = dbc.py_dict_drug.values()
 
-mainviz_xaxis = dbc.plotly_dict.keys()
-mainviz_yaxis = dbc.plotly_dict.values()
-settingviz_xaxis = dbc.plotly_dict_set.keys()
-settingviz_yaxis = dbc.plotly_dict_set.values()
+settingviz_xaxis = dbc.py_dict_set.keys()
+settingviz_yaxis = dbc.py_dict_set.values()
 
-mainviz = 'Number of appearances of slang in the last 1000 posts to /r/Drugs'
-settingviz = 'Number of appearances of medical and legal concerns in the last 1000 posts to /r/Drugs.'
-heatviz = 'Test heatmap of poly-drug posts.'
+viz_bar_drug = 'Number of appearances of drug types in the last 1000 /r/Drugs post titles.'
+viz_bar_set = 'Number of appearances of medical and legal concerns in the last 1000 posts to /r/Drugs.'
+
+viz_heat_drug = 'Heatmap of poly-drug mentions in the last 1000 /r/Drugs post titles.'
+viz_heat_set = 'Heatmap of the terms mentioned in the last 1000 /r/Drugs post titles.'
 
 
 def viz_bar(graph_name, xaxis, yaxis, filename):
-    """Creates a bar graph for plot.ly. Give it a name, the x and y as lists, and a plotly filename."""
+    """Creates a bar graph for plot.ly.
+    Takes title, the x and y as lists, and plotly filename."""
     data = Data([
         Bar(
             name=filename,
@@ -110,44 +102,23 @@ def viz_bar(graph_name, xaxis, yaxis, filename):
     fig = Figure(data=data, layout=layout)
     plot_url = viz.plot(fig, filename=filename)
 
-        # bar_viz = Data([graph_name])
-        # layout = Layout(
-        #     title=graph_name)
-        # fig = Figure(data=bar_viz, layout=layout)
-        # # viz.plot(bar_viz, filename=filename)
-        # viz.plot(fig, filename=filename)
 
+def viz_heat(graph_name, xyaxis, zaxis, filename):
+    data = Data([
+        Heatmap(
+            name=filename,
+            z=zaxis,
+            x=xyaxis,
+            y=xyaxis
+        )
+    ])
+    layout = Layout(
+        title=graph_name)
+    fig = Figure(data=data, layout=layout)
+    plot_url = viz.plot(fig, filename=filename)
 
-viz_bar(mainviz, mainviz_xaxis, mainviz_yaxis, 'skoomabot_main')
-viz_bar(settingviz, settingviz_xaxis, settingviz_yaxis, 'skoomabot_setting')
+viz_bar(viz_bar_drug, mainviz_xaxis, mainviz_yaxis, 'skoomabot_bar_drug')
+viz_bar(viz_bar_set, settingviz_xaxis, settingviz_yaxis, 'skoomabot_bar_set')
 
-# bar_graph = Bar(
-#     name=mainviz,
-#     x=mainviz_xaxis,
-#     y=mainviz_yaxis)
-# bar_viz = Data([bar_graph])
-# viz.plot(bar_viz, filename = 'skoomabot_main')
-
-# heat_graph = Heatmap(
-#     name=heatviz,
-#     # z=[[x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11], [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11], [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11]],
-#     z=[
-#         [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     ],
-#     x=mainviz_xaxis,
-#     y=mainviz_xaxis)
-# heat_viz = Data([heat_graph])
-# viz.plot(heat_viz, filename='skoomabot_heat')
+viz_heat(viz_heat_drug, mainviz_xaxis, dbc.py_matrix_drug, 'skoomabot_heat_drug')
+viz_heat(viz_heat_set, settingviz_xaxis, dbc.py_matrix_set, 'skoomabot_heat_set')
--- skoomabot_config.py
+++ skoomabot_config.py
@@ -8,6 +8,7 @@ Config file that contains the various:
 3. Functions to build plotly plot points.
 """
 
+from collections import OrderedDict
 import re
 import time
 
@@ -30,33 +31,34 @@ class Post:
         self.link = self.post.permalink
 
     # DO I WANT TO KEEP TRACK OF THE ACTUAL SLANG TERM?
-    # def _category(self, drug_dict, lookup):
-    def _category(self, term_dict, plotly_dict, lookup):
+    # def _category(self, term_dict, plotly_dict, plotly_matrix, lookup):
+    def _category(self, term_dict, plotly_dict, plotly_list, plotly_matrix, lookup):
         """Checks for slang terms in post title. Returns all matching drugs as a list.
         THIS IS INCOMPLETE."""
         list_of_matches = []
         lookup = lookup.split()
         for word in lookup:
             word = re.sub('[^0-9a-z\']', '', word)
-            # print word
             if len(word) > 2:
                 for key, value in term_dict.items():
-                # for key, value in drug_dict.items():
                     for v in value:
                         if re.match(r'{}'.format(v), word):
-                            # print 'Match: {} from {} category.'.format(word, key)
+                            # print 'Match: {} from {}.'.format(word, key)
                             if key not in list_of_matches:
                                 list_of_matches.append(key)
                             else:
                                 pass
         else:
             pass
-        for drug in list_of_matches:
-            self._insert_plot_point(plotly_dict, drug)
-            # self._insert_plot_point(drug_dict, drug)
+
+        for term in list_of_matches:
+            self._insert_plot_point(plotly_dict, term)
+
         if len(list_of_matches) > 1:
             print 'More than one drug mentioned.'
-
+            indicies_list = parse_poly(plotly_list, list_of_matches)
+            poly_coord = index_list_to_matrix_coord(indicies_list)
+            insert_heat_point(plotly_matrix, poly_coord)
         return list_of_matches
 
     def _match(self, dict):
@@ -66,21 +68,87 @@ class Post:
         else:
             return False
 
-    def _insert_plot_point(self, dict, drugtype):
+    def _insert_plot_point(self, x_axis_dict, term):
         """For every drug that's mentioned, uptick 1 to the key's value."""
-        # print 'test printing: {}'.format(drugtype)
         try:
-            dict[drugtype] += 1
+            x_axis_dict[term] += 1
         except (AttributeError, TypeError):
-            dict[drugtype] = 1
-
-    def _insert_heat_point(self, drugtype):
-        """For every poly-drug post, uptick the 2-combo by 1 point."""
+            x_axis_dict[term] = 1
 
 
 # TESTING OUT PLOTTING TO PLOT.LY
-plotly_dict = {'alcohol': None, 'caffeine': None, 'cannabis': None, 'nicotine': None, 'cocaine': None, 'ecstasy': None, 'heroin': None, 'lsd': None, 'meth': None, 'painkiller': None, 'salvia': None, 'otc': None, 'benzo': None, 'amphetamine': None, 'psilocybin': None}
-plotly_dict_set = {'location': None, 'deviant': None, 'party': None, 'health': None, 'physical': None, 'psychological': None, 'cost': None}
+# plotly_dict = {'alcohol': None, 'caffeine': None, 'cannabis': None, 'nicotine': None, 'cocaine': None, 'ecstasy': None, 'heroin': None, 'lsd': None, 'meth': None, 'painkiller': None, 'salvia': None, 'otc': None, 'benzo': None, 'amphetamine': None, 'psilocybin': None}
+# plotly_dict_set = {'location': None, 'deviant': None, 'party': None, 'health': None, 'physical': None, 'psychological': None, 'cost': None}
+
+
+##########################################
+# This should be moved into its own class.
+##########################################
+
+def create_matrix(dict_category):
+    """Takes a dict and returns matrix with x and y at dict legnth."""
+    print 'create_matrix dict: {}\n'.format(dict)
+    # dict_ord = OrderedDict(dict)
+    # print 'create_matrix dict post OrderedDict'.format(dict_ord)
+    # matrix_vals = dict_ord.keys()
+    matrix_vals = dict_category
+    matrix = [[0 for x in range(len(matrix_vals))] for x in range(len(matrix_vals))]
+    return matrix
+
+
+def dict_wo_vals(dict_w_vals):
+    """Takes a dict, returns new dict with None vals for all keys."""
+    # Ensure that OrderedDict is being properly used.
+    copy_dict = OrderedDict(dict_w_vals)
+    copy_dict = dict.fromkeys(copy_dict.keys(), None)
+    return copy_dict
+
+
+def parse_poly(drug_list, post_list):
+    """Takes a list of drugs and returns [indicies] based on drug_list.
+    Used for poly drug matches when parsing titles in posts."""
+    coord_list = []
+    for item in post_list:
+        coord_index = drug_list.index(item)
+        coord_list.append(coord_index)
+
+    print '\nThe indices for {}: {}'.format(post_list, coord_list)
+    return coord_list
+
+
+def index_list_to_matrix_coord(indicies_list):
+    """Takes a list of indicies, returns coordinates for the matrix.
+    Credit to /u/DarkGamanoid for the help."""
+    heatmap_coord_list = []
+    for item in indicies_list:
+        try:
+            index = indicies_list.index(item)
+            for other_items in indicies_list:
+                if other_items is not item:
+                    position = [item, other_items]
+                    heatmap_coord_list.append(position)
+                # print 'pos: {}'.format(position)
+        except IndexError:
+            pass
+
+    print '\nCoordinates to uptick: {}'.format(heatmap_coord_list)
+    return heatmap_coord_list
+
+
+def insert_heat_point(matrix, coords_list):
+    """For every ordered pair in list, uptick 1 to (x, y) in heatmap matrix.
+    Used in poly-drug posts, uptick each 2-combo by 1 point."""
+    for coord in coords_list:
+        try:
+            xaxis = coord[0]
+            yaxis = coord[1]
+            matrix[xaxis][yaxis] += 1
+        except (AttributeError, TypeError):
+            matrix[xaxis][yaxis] = 1
+
+
+# def sort_dict(dict_unsorted):
+#     for key, value in sorted(dict_unsorted.items())
 
 checked_post = {}
 checked_post.setdefault('pos', [])
@@ -102,44 +170,57 @@ painkiller = ['oxy', 'codone', 'dilaudid', 'perc']
 salvia = ['salvia']
 otc = ['maoi', 'benadryl', 'dxm']
 benzo = ['benzo', 'valium', 'xanax', 'klonopin', 'ativan']
-amphetamine = ['amph', 'adderall', 'vyvanse', 'dex', 'concerta', 'ritalin', 'focalin']
 psilocybin = ['shroom']
+amphetamine = ['amph', 'adderall', 'vyvanse', 'dex', 'concerta', 'ritalin', 'focalin']
 
 drug_dict = {}
+drug_dict = OrderedDict(drug_dict)
 drug_dict['alcohol'] = alcohol
+drug_dict['amphetamine'] = amphetamine
+drug_dict['benzo'] = benzo
 drug_dict['caffeine'] = caffeine
 drug_dict['cannabis'] = cannabis
-drug_dict['nicotine'] = nicotine
 drug_dict['cocaine'] = cocaine
 drug_dict['ecstasy'] = ecstasy
 drug_dict['heroin'] = heroin
 drug_dict['lsd'] = lsd
 drug_dict['meth'] = meth
-drug_dict['painkiller'] = painkiller
-drug_dict['salvia'] = salvia
+drug_dict['nicotine'] = nicotine
 drug_dict['otc'] = otc
-drug_dict['benzo'] = benzo
-drug_dict['amphetamine'] = amphetamine
+drug_dict['painkiller'] = painkiller
 drug_dict['psilocybin'] = psilocybin
+drug_dict['salvia'] = salvia
+# drug_dict = sorted(drug_dict.keys())
 
 # Use a dict for lists of locations, health, and pos/neg descriptors mentioned.
 location = ['home', 'school', 'job', 'work', 'dispensary']
-deviant = ['prison', 'jail', 'police', 'cop', 'officer', 'arrest', 'undercover']
-party = ['rave', 'party', 'club', 'edm', 'concert', 'festival']
+legal = ['prison', 'jail', 'police', 'cop', 'officer', 'arrest', 'undercover', 'raid']
+party = ['rave', 'party', 'club', 'edm', 'concert', 'festival', 'bar']
 health = ['addict', 'reaction', 'safe', 'danger', 'withdrawl', 'prescribe']
-physical = ['allergic', 'death', 'die', 'nausea', 'sleep', 'tolerance', 'toxic']
+physical = ['allerg', 'death', 'die', 'nausea', 'sleep', 'tolerance', 'toxic']
 psychological = ['anxiety', 'ego', 'depress', 'dysfunction', 'coping', 'mental', 'neuro', 'scare', 'therapy']
 cost = ['cost', 'price', 'afford', 'money', 'sell', 'hustle', 'deal']
 # question = ['how to']
 # politics = ['dispensary']
 
 setting_dict = {}
-setting_dict['location'] = location
-setting_dict['deviant'] = deviant
-setting_dict['party'] = party
+setting_dict = OrderedDict(setting_dict)
+setting_dict['cost'] = cost
 setting_dict['health'] = health
+setting_dict['legal'] = legal
+setting_dict['location'] = location
+setting_dict['party'] = party
 setting_dict['physical'] = physical
 setting_dict['psychological'] = psychological
-setting_dict['cost'] = cost
+# setting_dict = sorted(setting_dict.keys())
+
 # pos_high = []
 # neg_high = []
+
+py_list_drug = drug_dict.keys()
+py_dict_drug = dict_wo_vals(drug_dict)
+py_matrix_drug = create_matrix(drug_dict)
+
+py_list_set = setting_dict.keys()
+py_dict_set = dict_wo_vals(setting_dict)
+py_matrix_set = create_matrix(setting_dict)
